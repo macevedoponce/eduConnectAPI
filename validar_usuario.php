4cc5c017ab7b448149e0318fef1dba2254a87a03
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-05-23 21:03:46 -0500

    modificando API validar_usuario

--- validar_usuario.php
+++ validar_usuario.php
@@ -1,3 +1,4 @@
+
 <?php
 
 include 'conexion.php';
@@ -5,19 +6,16 @@ include 'conexion.php';
 $json=array();
 
 if(isset($_GET["usuario"])){
-  //echo password_hash("password123", PASSWORD_DEFAULT)."\n";
-    
+
     $dni=$_GET['usuario'];
     $password=$_GET['contrasena'];
 
     $sentencia=$conexion->prepare("SELECT * FROM usuario WHERE dni=?");
-    $sentencia->bind_param('s',$dni,);
+    $sentencia->bind_param('s',$dni);
     $sentencia->execute();
     $resultado = $sentencia->get_result();
     $fila = $resultado->fetch_assoc();
-    $pass = $fila["password"];
-    //echo $pass;
-    if($fila && password_verify($password,$pass)){
+    if($fila && password_verify($password,$fila['password'])){
        //echo json_encode($fila,JSON_UNESCAPED_UNICODE);
        $json['usuario'][]=$fila;
       }
