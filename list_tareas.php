1cb83cc3ee99d0b184c56b719c906557588be62f
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-05-30 04:40:03 -0500

    list_tareas_alumnos, list_cursos_alumnos

--- list_tareas.php
+++ list_tareas.php
@@ -5,10 +5,36 @@ $json=array();
 if(isset($_GET["id_curso"])){
 
         $id_curso = $_GET['id_curso'];
-        
-		$consulta="SELECT * FROM tarea WHERE curso_id ='{$id_curso}' ORDER BY id DESC";
+
+		if(isset($_GET["id_usuario"])){
+			$id_usuario = $_GET['id_usuario'];
+			//$consulta="SELECT * FROM tarea t INNER JOIN entrega_tarea et WHERE et.id_tarea = t.id AND t.curso_id ='{$id_curso}' AND t.estado_id = 1 AND et.id_usuario = '{$id_usuario}' ORDER BY t.id DESC;";
+			$consulta="SELECT * 
+			FROM tarea t 
+			LEFT JOIN entrega_tarea et ON et.id_tarea = t.id AND et.id_usuario = '{$id_usuario}'
+			WHERE t.curso_id ='{$id_curso}' AND t.estado_id = 1 
+			ORDER BY t.id DESC;
+			";
+			$resultado=mysqli_query($conexion,$consulta);
 		
-		$resultado=mysqli_query($conexion,$consulta);
+			while($registro=mysqli_fetch_array($resultado)){
+				$result["id"]=(int)$registro['id'];
+				$result["titulo"]=$registro['titulo'];
+				$result["descripcion"]=$registro['descripcion'];
+				$result["fecha_limite"]=$registro['fecha_limite'];
+				$result["retroalimentacion"]=$registro['retroalimentacion'];
+				$result["url_trabajo"]=$registro['url_trabajo'];
+				$result["nota"]=(int)$registro['nota'];
+				$json['tareas'][]=$result;
+				//echo $registro['id'].' - '.$registro['nombre'].'<br/>';
+			}
+			mysqli_close($conexion);
+			header('Content-Type: application/json');
+			echo json_encode($json);
+
+		}else{
+			$consulta="SELECT * FROM tarea WHERE curso_id ='{$id_curso}' ORDER BY id DESC";
+			$resultado=mysqli_query($conexion,$consulta);
 		
 		while($registro=mysqli_fetch_array($resultado)){
 			$result["id"]=(int)$registro['id'];
@@ -23,6 +49,9 @@ if(isset($_GET["id_curso"])){
 		mysqli_close($conexion);
 		header('Content-Type: application/json');
 		echo json_encode($json);
+		}
+        
+		
     }else{
         $resulta["id"]=0;
         $resulta["tareas"]='No registra Cursos';
